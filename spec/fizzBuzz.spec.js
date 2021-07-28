c962e2ea3e2226e1ec25f8dda2e4c1ac69bee163
maxarvid <max.arvid.anderson@gmail.com>
2021-07-28 16:22:38 +0200

    change indent to 2 spaces instead of 4

--- spec/fizzBuzz.spec.js
+++ spec/fizzBuzz.spec.js
@@ -2,23 +2,23 @@ const { expect } = require('chai');
 const FizzBuzz = require('../src/fizzBuzz.js')
 
 describe('FizzBuzz', () => {
-    let game
+  let game
 
-    before(() => {
-        game = new FizzBuzz()
-    })
+  before(() => {
+    game = new FizzBuzz()
+  })
 
-    it('is expected to return 1 if given 1', () => {
-        expect(game.play(1)).to.eql(1)
-    })
-    it('is expected to return fizz if given 3', () => {
-        expect(game.play(3)).to.eql('fizz')
-    })
-    it('is expected to return buzz if given 5', () => {
-        expect(game.play(5)).to.eql('buzz')
-    })
-    it('is expected to return fizzbuzz if given 15', () => {
-        expect(game.play(15)).to.eql('fizzbuzz')
-    })
-    
+  it('is expected to return 1 if given 1', () => {
+    expect(game.play(1)).to.eql(1)
+  })
+  it('is expected to return fizz if given 3', () => {
+    expect(game.play(3)).to.eql('fizz')
+  })
+  it('is expected to return buzz if given 5', () => {
+    expect(game.play(5)).to.eql('buzz')
+  })
+  it('is expected to return fizzbuzz if given 15', () => {
+    expect(game.play(15)).to.eql('fizzbuzz')
+  })
+
 })
